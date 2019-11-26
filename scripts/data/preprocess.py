3a397a772341ee377639ac956985658596d3c3cf
enhuiz <niuzhe.nz@outlook.com>
2019-11-26 21:30:50 +0800

    update preprocessing

--- scripts/data/preprocess.py
+++ scripts/data/preprocess.py
@@ -5,7 +5,10 @@ import re
 import numpy as np
 import argparse
 import pandas as pd
-from nltk.tokenize import word_tokenize
+import tqdm
+import spacy
+
+nlp = spacy.load('en')
 
 
 def get_args():
@@ -18,20 +21,20 @@ def get_args():
 def process_sentence(s):
     # make all characters lower case
     s = s.strip().lower()
-    # convert year-old to year old, so that there is smaller vocab
-    s = s.replace('-', ' ')
     # change ` to '
     s = s.replace('‘', "'")
     # change ‘ to '
     s = s.replace('’', "'")
-    # tokenize, convert he's to he 's
-    s = ' '.join(word_tokenize(s))
     # remove comma inside number
     s = re.sub(r"(\d+?),", r"\1", s)
     # convert number to digits, 123 -> 1 2 3,
     s = re.sub(r"([0-9])", r" \1 ", s).strip()
+    # lemmatization
+    s = ' '.join([token.lemma_ for token in nlp(s)])
     # replace % with percent
     s = s.replace('%', 'percent')
+    # remove extra space
+    s = ' '.join(s.split(' '))
     return s
 
 
@@ -46,7 +49,7 @@ def process(df):
     del df['original']
 
     # process the sentences
-    df['text'] = df['text'].apply(process_sentence)
+    df['text'] = df['text'].progress_apply(process_sentence)
 
     df['text'] = df['text'].apply(lambda s: s.replace('swapiii', '<swap3>')
                                   .replace('swapii', '<swap2>')
@@ -57,6 +60,7 @@ def process(df):
 
 def main():
     args = get_args()
+    tqdm.tqdm.pandas()
     for split in ['train', 'dev']:
         path = os.path.join(args.root, '{}.csv'.format(split))
         df = pd.read_csv(path)
