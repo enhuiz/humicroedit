4af61efc4c5706c01ebd189e1749a704cfbfc7b5
enhuiz <niuzhe.nz@outlook.com>
2019-11-27 18:23:29 +0800

    add kg process

---
+++ scripts/data/process_kg.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import numpy as np
+import argparse
+import pandas as pd
+import tqdm
+import spacy
+
+nlp = spacy.load('en')
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type=str, default='data/humicroedit/task-1')
+    args = parser.parse_args()
+    return args
+
+
+def process_sentence(s):
+    # make all characters lower case
+    s = s.strip().lower()
+    # change ` to '
+    s = s.replace('‘', "'")
+    # change ‘ to '
+    s = s.replace('’', "'")
+    # remove comma inside number
+    s = re.sub(r"(\d+?),", r"\1", s)
+    # convert number to digits, 123 -> 1 2 3,
+    s = re.sub(r"([0-9])", r" \1 ", s).strip()
+    # lemmatization
+    s = ' '.join([token.lemma_ for token in nlp(s)])
+    # replace % with percent
+    s = s.replace('%', 'percent')
+    # remove extra space
+    s = ' '.join(s.split(' '))
+    return s
+
+
+def process(df):
+    cols = [col for col in df.columns
+            if 'original-' in col or 'edited-' in col]
+
+    def tokenize(col): return '<{}>'.format(col.lower())
+
+    def process_row(row):
+        text = [row['text']]
+        for col in cols:
+            sentences = eval(row[col])
+            s = next((s for s in sentences if s != 'none'), 'none')
+            text.append(tokenize(col) + ' ' + process_sentence(s))
+        text = ' '.join(text)
+        return text
+
+    df['text'] = df.progress_apply(process_row, axis=1)
+
+    for col in cols:
+        del df[col]
+
+    return df
+
+
+def main():
+    args = get_args()
+    tqdm.tqdm.pandas()
+    for split in ['train', 'dev']:
+        path = os.path.join(args.root,
+                            '{}.preprocessed.kg.csv'.format(split))
+        df = pd.read_csv(path)
+        df = process(df)
+        outpath = os.path.join(args.root,
+                               split + '.preprocessed.kg.processed.csv')
+        df.to_csv(outpath, index=None)
+        print(df.head()['text'])
+
+
+if __name__ == "__main__":
+    main()
